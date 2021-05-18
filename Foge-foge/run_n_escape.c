ea388e2dd9b764f25f14e888a87b38419cfd9fa4
Tawfik Metwally <tawfikaly02@gmail.com>
2021-05-18 04:43:51 -0300

    Allocatemap and creat a specific files

--- Foge-foge/run_n_escape.c
+++ Foge-foge/run_n_escape.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "RNE_functions.h"
+
+char **map;
+int rows;
+int columns;
 
 int main()
 {
+    readmap();
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", map[i]);
+    }
+
+    freemap();
+    return 0;
+}
+
+void readmap()
+{
     FILE *f;
-    char map[5][10 + 1];
 
     f = fopen("map.txt", "r");
 
@@ -14,17 +31,32 @@ int main()
         exit(1);
     }
 
+    fscanf(f, "%d %d", &rows, &columns);
+    allocatemap();
+
     for (int i = 0; i < 5; i++)
     {
         fscanf(f, "%s", map[i]);
     }
 
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%s\n", map[i]);
-    }
-
     fclose(f);
+}
 
-    return 0;
+void allocatemap()
+{
+    map = malloc(sizeof(char *) * rows);
+
+    for (int i = 0; i < rows; i++)
+    {
+        map[i] = malloc(sizeof(char) * columns + 1);
+    }
+}
+
+void freemap()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        free(map[i]);
+    }
+    free(map);
 }
